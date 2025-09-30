09652689a98003998ab44a40a3eb51daa729ff3e
EvgeniyPryanichnikov <pryanichnikov.eugeny@yandex.ru>
2025-09-30 23:30:47 +0400

    add: HeaderNav

--- src/core/components/layout/HeaderNav.vue
+++ src/core/components/layout/HeaderNav.vue
@@ -16,6 +16,7 @@ const navItems = [
   {id: 1, title: 'Места'},
   {id: 2, title: 'Карта'},
   {id: 3, title: 'Галлерея'},
+  {id: 4, title: 'Погода'},
 ]
 </script>
 
@@ -24,11 +25,18 @@ const navItems = [
   @import '@/assets/styles/abstracts/_mixins.scss';
 
   .header__nav {
+    box-sizing: border-box;
     position: fixed;
     display: flex;
-    width: 100%;
+    gap: 16px;
+    width: calc(100% - 24px);
     padding: 20px;
     top: 10px;
-    left: 10px;
+    left: 0;
+    background: rgba(255, 255, 255, 0.5);
+    backdrop-filter: blur(10px);
+    border-radius: 18px;
+    margin-left: 12px;
+    margin-right: 12px;
   }
 </style>
